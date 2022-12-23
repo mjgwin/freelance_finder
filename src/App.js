5a171c8225121da62e208a364763a6a13cfa3d65
mjgwin <micahjgwin@gmail.com>
2022-12-23 13:28:52 -0600

    started building out navbar and themes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
+const mainTheme = createTheme({
+  palette: {
+    background: {
+      default: "#222222",
+    },
+    primary: {
+      main: "#0971f1", //this one matters for actually changing the color
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <ThemeProvider theme={mainTheme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/tutorials" element={<Tutorials />}/>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} /> */}
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import {Link} from 'react-router-dom'
+import React from "react";
+
+const Navbar = () => {
+    const pages = ["Home", "Create", "Browse"];
+
+  return(
+    <div>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <Typography noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+                            {/* <img src={Chad} style={{ width: 60, height: 40 }} alt="CHAD" /> */}
+                            Freelance Finder!
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    component={Link}
+                                    to={page === "Home" ? "/" : `/${page}`}
+                                    sx={{ my: 2, color: 'white', display: 'block', marginX: 2}}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    </div>
+  )
+};
+
+export default Navbar;
