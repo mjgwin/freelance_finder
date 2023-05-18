e4d34ca57f41eb74904d4bf75b01b09d76f84b85
mjgwin <micahjgwin@gmail.com>
2023-05-17 23:09:31 -0500

    added dynamo table resource, working on lambda

--- lambda_code/api_lambda.js
+++ lambda_code/api_lambda.js
@@ -1,7 +1,59 @@
 import { DynamoDB } from "aws-sdk";
 
-const client = new DynamoDB()
+const TABLE_NAME = process.env.TABLE_NAME
+
+const client = new DynamoDB({region: "us-east-1"})
+
+function generate_user_id(){
+    //todo
+}
+
+function process_user(routeKey, body, pathParameters) {
+    let return_body = {outcome: "success", contents: {}}
+
+    try {
+        if(routeKey.includes("PUT /users")){
+            let params = {
+                TableName: TABLE_NAME,
+                Item: {
+                    "user_id": {
+                        S: generate_user_id()
+                    },
+                    "user_email": {
+                        S: body.user_email
+                    },
+                    "user_password": {
+                        S: body.user_password
+                    },
+                }
+            }
+            client.putItem(params, function(err, data){
+                if (err){ 
+                    console.log(`Error adding user: ${body.user_email} to table: ${TABLE_NAME}`)
+                    return_body.outcome = "failure"
+                }
+                else console.log(`Added user with email: ${body.user_email}`)
+            })
+        }else if(routeKey.includes("GET /users")){
+            let params = {TableName: TABLE_NAME}
+            client.scan(params, function(err, data){
+                if (err){ 
+                    console.log(`Error scanning table: ${TABLE_NAME}`)
+                    return_body.outcome = "failure"
+                }
+                else return_body.contents = data.Items
+            })
+            //todo: handle GET /users/{id}
+        }else if(routeKey.includes("DELETE /users/{id}")){
+            //todo
+        }else{
+            console.log(`Unsupported route key: ${routeKey}`)
+        }
+    }catch(error){
+        console.log(`Unexpected error: ${error}`)
+    }
+}
 
 module.exports.handler = (event, context) => {
-
+    process_user(event.routeKey, JSON.parse(event.body), event.pathParameters)
 }
--- main.tf
+++ main.tf
@@ -18,3 +18,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_dynamodb_table" "freelance-finder-user-table" {
+  name           = "freelance-finder-user-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "user_email"
+    type = "S"
+  }
+
+  attribute {
+    name = "user_password"
+    type = "S"
+  }
+
+}
+
